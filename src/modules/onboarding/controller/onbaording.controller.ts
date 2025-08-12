5955fe50512b1d374204d5b2a664875c45f35be8
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-08-12 17:08:25 +0530

    Changes

--- src/modules/onboarding/controller/onbaording.controller.ts
+++ src/modules/onboarding/controller/onbaording.controller.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/user", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World change");
 });
 
 export default router;
